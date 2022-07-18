82f5e43e371ffac1a8e1949160a989fed613347b
snowysecret-15 <108406301+snowysecret-15@users.noreply.github.com>
2022-07-18 22:06:56 +0530

    Add files via upload

---
+++ 7-july-2022/Day9/HTML/JS/day9.js
@@ -0,0 +1,19 @@
+function ddd(){
+    var imgsrc= document.getElementById('02').getAttribute('src');
+    document.getElementById('01').src=imgsrc;
+    }
+    
+    function ddd1(){
+        var imgsrc= document.getElementById('03').getAttribute('src');
+        document.getElementById('01').src=imgsrc;
+    }
+    
+    function ddd2(){
+        var imgsrc= document.getElementById('04').getAttribute('src');
+        document.getElementById('01').src=imgsrc;
+    }
+    
+        function ddd3(){
+            var imgsrc= document.getElementById('05').getAttribute('src');
+            document.getElementById('01').src=imgsrc;
+    }
